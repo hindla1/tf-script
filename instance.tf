5d75b87aea01bd88885805bff737d5d5134a9ad7
hindla1 <indlaharibabu@gmail.com>
2020-08-07 07:10:41 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,8 +1,7 @@
 resource "aws_key_pair" "mykey" {
 
      key_name = "my-own-key"
-     public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCrTIWNdQIGkZuiym5L+l4ZJ6OPQkvUsJltFRIqqRJ7O+f7Uzl9xOo9viX6tJ2ebajIODd5cnEmtUYvOxz7fjk44hL5ptfZCWX0Wx/V5+4bOMToEmpXQrZiIpaxlKHcZzUEAkgMCRH/LhhCHsUOyiH3ogYBs9uZkF294dEhaj5iM0TJ5sLEn0wzMISdFjAe37492J7NLNyx/N7c5hwO2Xj2677RbZS+85ZLILfTVwkfphwHt9aubmQ/amlqaKLoBTqDSGvZynIDQPbUAz4dOlWNDYVVfCYZIeSps59NwF255HkKseaN6eEZmK+aWQaUy5Qs8rfPODCqmp2tCzjJrDyEnM3gAACDKvTngio6CnE6St2Ieb1TPbLARgPKF8C12s5wOPtOjWN5GtlBlAOqWsqucQIlz7XlDVYbwM7vgm/HQcJoiSbnTc+/9yFZZwv2gBBIpPaV9bPYWyqcqrqTVcdoQyjxTxAuPHp/g4Q22XUns2x9HBNIQfpIZueyOi/vHCk= i353111@FVFVT124J1WV" 
-
+     public_key = file("my_key.pub")
 }
 
 
